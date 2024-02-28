be6ac243763a19b54e0c9edf93bf2b41258e2982
rodri-95 <159568674+rodri-95@users.noreply.github.com>
2024-02-28 17:21:32 -0300

    loja

--- T-Shoes/js/script.js
+++ T-Shoes/js/script.js
@@ -11,11 +11,27 @@ const generateCard = async () => {
     const products = await getProducts()
 
     products.map(product => {
-        // console.log(product)
         let card = document.createElement('div')
         card.classList.add('card__produto')
-        console.log(card)
+        
+        card.innerHTML = `
+        <figure>
+            <img src="images/${product.image}" alt="${product.product_name}" />
+        </figure>
 
-})
+        <div class="card__produto_detalhes">
+            <h4>${product.product_name}</h4>
+            <h5>${product.product_model}</h5>
+        </div>
+
+        <h6>R$ ${product.price}</h6>
+        `
+
+        const listaProdutos = document.querySelector('.lista__produtos')
+        listaProdutos.appendChild(card)
+
+    })
+   
+}
 
 generateCard()
